fe343443586063e486e02e81df85d86e421c0652
mathiassmichno <mmichn13@student.aau.dk>
2015-08-15 15:17:36 +0200

    angular seperation plus bonus info integrated

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('myapp', []);
+
+app.controller('CSGO_server_Ajax', function ($scope, $http) {
+    $http({method: 'GET', url: 'http://api.michno.me:3000/gameserverquery/csgo/csgoserver.michno.me'}).success(function(data) {
+        $scope.csgoData = data;
+        $scope.csgoLoaded = true;
+    });
+});
+
+app.controller('mumble_server_Ajax', function ($scope, $http) {
+    $http({method: 'GET', url: 'http://api.michno.me:3000/gameserverquery/mumbleping/mumble.michno.me'}).success(function(data) {
+        $scope.mumbleData = data; // response data 
+        $scope.mumbleLoaded = true;
+    });
+});
+
+app.controller('ts3_server_Ajax', function ($scope, $http) {
+    $http({method: 'GET', url: 'http://api.michno.me:3000/gameserverquery/ts3/ts.michno.me/9987'}).success(function(data) {
+        $scope.ts3Data = data; // response data 
+        $scope.ts3Loaded = true;
+    });
+});       
